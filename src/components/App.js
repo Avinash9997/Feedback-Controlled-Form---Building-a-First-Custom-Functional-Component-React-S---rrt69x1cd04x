f514ab6f069690a5639eb73ca7245512b6ea4ab2
Avinash9997 <126712293+Avinash9997@users.noreply.github.com>
2023-05-27 22:10:00 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,27 @@ import React, { useState } from 'react'
 import '../styles/App.css';
 
 const App = () => {
-  const [rating, setRating] = useState();
-  const [comment, setComment] = useState();
-  const [commentError, setCommentError] = useState();
+  const [rating, setRating] = useState(5);
+  const [comment, setComment] = useState("");
+  const [commentError, setCommentError] = useState(false);
+
+
+  const handleRatingChange=(event)=>{
+    setRating(event.target.value);
+  }
+  const handleCommentChange=(event)=>{
+    setComment(event.target.value);
+    setCommentError(event.target.value.length<=5)
+  }
+ 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+ 
+  };
 
   return (
     <div id="main">
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor='rating'>Rating: </label>
           <input 
@@ -16,15 +30,19 @@ const App = () => {
             min="1"
             max="10"
             id="rating"
+            value={rating}
+            onChange={handleRatingChange}
           />
-          <span className='rating'>rating</span>
+          <span className='rating' >{rating}</span>
         </div>
         <div>
           <label htmlFor='comment'>Comment: </label>
           <textarea 
             id='comment'
+            value={comment}
+            onChange={handleCommentChange}
           />
-          <p style={{ color: 'red' }} className="comment-error">Comment must be atleast 5 characters.</p>
+         {commentError &&  <p style={{ color: 'red' }} className="comment-error">Comment must be atleast 5 characters.</p>}
         </div>
         <button type='submit'>Submit</button>
       </form>
@@ -34,3 +52,62 @@ const App = () => {
 
 
 export default App;
+
+
+
+// import React, { useState } from 'react'
+// import '../styles/App.css';
+
+// const App = () => {
+//  const [rating, setRating] = useState(5);
+//   const [comment, setComment] = useState('');
+//   const [commentError, setCommentError] = useState(false);
+
+
+//   const handleRatingChange = (event) => {
+//     setRating(parseInt(event.target.value));
+//   };
+
+//   const handleCommentChange = (event) => {
+//     setComment(event.target.value);
+//     setCommentError(event.target.value.length <= 5); // Check comment length and set error state
+//   };
+
+//   const handleSubmit = (event) => {
+//     event.preventDefault();
+//     // Perform submit actions here
+//   };
+
+//   return (
+//     <div id="main">
+//       <form onSubmit={handleSubmit}>
+//         <div>
+//           <label htmlFor='rating'>Rating: </label>
+//           <input 
+//             type="range"
+//             min="1"
+//             max="10"
+//             id="rating"
+//             value={rating}
+//             onChange={handleRatingChange}
+            
+//           />
+//           <span className='rating'>{rating}</span>
+//         </div>
+//         <div>
+//           <label htmlFor='comment'>Comment: </label>
+//           <textarea 
+//             id='comment'
+//             value={comment}
+//             onChange={handleCommentChange}
+//           />
+//           {commentError && <p style={{ color: 'red' }} className="comment-error">Comment must be atleast 5 characters.</p>}
+//         </div>
+//         <button type='submit'>Submit</button>
+//       </form>
+//     </div>
+//   )
+// }
+
+
+// export default App;
